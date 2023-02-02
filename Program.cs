b74da6e1a8d1bbf0dcd95741abcbadf52d6fe8c9
Darya <dasha.890@yandex.ru>
2023-02-02 19:36:45 +0500

    второе задание

--- Program.cs
+++ Program.cs
@@ -44,7 +44,59 @@ int[,] DescendingRows(int[,] inArray)
     }
     return inArray;
 }
+string FindNumber(int[,] inArray, int Numb)
+{
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] == Numb)
+            {
+                x = i;
+                y = j;
+            }
+
+        }
+    }
+    if (x == 0 && y == 0)
+    {
+        return ($"Элемента {Numb} в массиве нет.");
+    }
+    else
+    {
+        return ($"Элемент {Numb} стоит в массиве на {x} строке в {y} столбце");
+    }
+
+}
+int MinSuminRows(int[,] inArray)
+{
+    int min = 0;
+    int row = 0;
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        min = min + inArray[0, i];
+    }
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            sum = sum + inArray[i, j];
+        }
+        if (sum < min)
+        {
+            min = sum;
+            row = i + 1; //Выводим номер строки, понятный для пользователя (если бы строки начинали нумероваться с 1)
+        }
+
+    }
+    return row;
+}
 //Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+/*
 Console.WriteLine("Введите количество строк массива: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов массива: ");
@@ -53,8 +105,17 @@ int[,] NewArray = GetArray(columns, rows, 0, 100);
 PrintArray(NewArray);
 Console.WriteLine("Измененный массив: ");
 PrintArray(DescendingRows(NewArray));
+*/
 
 //Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Введите количество строк массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+int[,] NewArray = GetArray(columns, rows, 0, 10);
+PrintArray(NewArray);
+Console.WriteLine($"Строка с наименьшей суммой элементов под номером: {MinSuminRows(NewArray)}");
 
 //Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
